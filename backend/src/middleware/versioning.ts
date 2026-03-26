2cc484122bd76822e9bc17b535430b8f3b3da52c
Samuel1505 <oluwakoredeakinola@gmail.com>
2026-03-26 08:12:56 +0100

    feat: implement API versioning

---
+++ backend/src/middleware/versioning.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+
+declare global {
+  namespace Express {
+    interface Request {
+      apiVersion?: string;
+    }
+  }
+}
+
+export const versionMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const headerVersion = req.headers['api-version'] || req.headers['x-api-version'] || req.headers['accept-version'];
+  let version = 'v1';
+
+  if (headerVersion) {
+    version = `v${headerVersion.toString().replace(/^v/i, '')}`;
+  } else {
+    const match = req.originalUrl.match(/^\/api\/(v\d+)\//);
+    if (match) {
+      version = match[1];
+    }
+  }
+
+  req.apiVersion = version;
+  res.setHeader('X-API-Version', version);
+
+  next();
+};
